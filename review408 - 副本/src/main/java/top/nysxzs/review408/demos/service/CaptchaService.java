5d814f65e6ee6556b47f35cc6246dafcbc67b462
3t3rn41 <116739736+3t3rn41@users.noreply.github.com>
2024-07-15 19:24:23 +0800

    Delete review408 - 副本 directory

--- review408 - 副本/src/main/java/top/nysxzs/review408/demos/service/CaptchaService.java
+++
@@ -1,9 +0,0 @@
-package top.nysxzs.review408.demos.service;
-
-import java.io.IOException;
-
-public interface CaptchaService {
-    byte[] generateCaptcha(String username) throws IOException;
-    String verifyCode(String username,String verifyCode);
-
-}
